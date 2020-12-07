116ab87927f09031ba0880e3c6f9518bab277abd
Aditya140294 <75359915+Aditya140294@users.noreply.github.com>
2020-12-07 12:43:24 +0530

    Update Demo1.java

--- gitdemo/src/gitdemo/Demo1.java
+++ gitdemo/src/gitdemo/Demo1.java
@@ -3,8 +3,8 @@ package gitdemo;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("first git project");
-		
-		
+	
+		System.out.println(" second line first git project");
 		
 	}
 	
